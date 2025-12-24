d8086c8b9ffe1ac5252dda6d66dd25c75ddcd7df
shivanandgond-ds <shivanand.gond@artefact.com>
2025-12-24 14:12:01 +0300

    Update start_api.sh

--- start_api.sh
+++ start_api.sh
@@ -3,12 +3,6 @@
 # Exit on error
 set -e
 
-# Initialize micromamba
-source /usr/local/bin/micromamba shell hook
-
-# Activate base
-micromamba activate base
-
 # Start the API server
 echo "Starting API server"
 uvicorn api:app --host 0.0.0.0 --port 8080 --workers 1 --backlog 10
